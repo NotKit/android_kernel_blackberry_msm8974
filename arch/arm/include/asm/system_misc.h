509549847e3bebbc9a8caa52ef5447c8ec518a0b
TheKit <thekit@disroot.org>
2026-01-13 09:45:21 +0200

    arch: arm: Port socinfo and add arch_read_hardware_id
    - Replace socinfo.c with 3.18 compatible version (uses sys_soc.h instead of sysdev.h)
    - Add arch_read_hardware_id declaration to system_misc.h
    - Add arch_read_hardware_id definition to process.c
    
    Remaining undefined references to address:
    - MSM platform functions (krait_power_init, tsens_tm_init_driver, etc.)
    - Sound compress API (snd_compress_new)
    - WCD calibration (wcdcal_get_fw_cal, taiko_reg_readable, etc.)
    - MMC RPM functions (mmc_rpm_hold, mmc_rpm_release)

--- arch/arm/include/asm/system_misc.h
+++ arch/arm/include/asm/system_misc.h
@@ -13,8 +13,9 @@ extern void cpu_init(void);
 void soft_restart(unsigned long);
 extern void (*arm_pm_restart)(enum reboot_mode reboot_mode, const char *cmd);
 extern void (*arm_pm_idle)(void);
+extern char* (*arch_read_hardware_id)(void);
 
-#define UDBG_UNDEFINED	(1 << 0)
+#define UDBG_UNDEFINED  (1 << 0)
 #define UDBG_SYSCALL	(1 << 1)
 #define UDBG_BADABORT	(1 << 2)
 #define UDBG_SEGV	(1 << 3)
