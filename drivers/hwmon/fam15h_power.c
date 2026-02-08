069069b8f351b8181c31179420913019f92add91
TheKit <thekit@disroot.org>
2026-02-08 12:25:05 +0200

    drivers/hwmon: Add hardware monitor modifications
    Source: 705c69cf1d87 (original MSM8974 BSP cherry-pick)
    Files: 11 added from BSP source
    Group: 62_hwmon (Layer 1: Clean BSP)
    
    This commit adds the original MSM8974/BB Passport BSP code as ported
    from the 3.4 kernel. Some code may use deprecated 3.4-era APIs;
    subsequent adaptation commits update these for 3.18 compatibility.

--- drivers/hwmon/fam15h_power.c
+++ drivers/hwmon/fam15h_power.c
@@ -31,6 +31,9 @@ MODULE_DESCRIPTION("AMD Family 15h CPU processor power monitor");
 MODULE_AUTHOR("Andreas Herrmann <herrmann.der.user@googlemail.com>");
 MODULE_LICENSE("GPL");
 
+/* Family 16h Northbridge's function 4 PCI ID */
+#define PCI_DEVICE_ID_AMD_16H_NB_F4	0x1534
+
 /* D18F3 */
 #define REG_NORTHBRIDGE_CAP		0xe8
 
@@ -174,7 +177,11 @@ static int fam15h_power_resume(struct pci_dev *pdev)
 #define fam15h_power_resume NULL
 #endif
 
+<<<<<<< HEAD
+static void __devinit fam15h_power_init_data(struct pci_dev *f4,
+=======
 static void fam15h_power_init_data(struct pci_dev *f4,
+>>>>>>> android-3.18
 					     struct fam15h_power_data *data)
 {
 	u32 val;
@@ -232,9 +239,13 @@ static int fam15h_power_probe(struct pci_dev *pdev,
 
 static const struct pci_device_id fam15h_power_id_table[] = {
 	{ PCI_VDEVICE(AMD, PCI_DEVICE_ID_AMD_15H_NB_F4) },
+<<<<<<< HEAD
+	{ PCI_VDEVICE(AMD, PCI_DEVICE_ID_AMD_16H_NB_F4) },
+=======
 	{ PCI_VDEVICE(AMD, PCI_DEVICE_ID_AMD_15H_M30H_NB_F4) },
 	{ PCI_VDEVICE(AMD, PCI_DEVICE_ID_AMD_16H_NB_F4) },
 	{ PCI_VDEVICE(AMD, PCI_DEVICE_ID_AMD_16H_M30H_NB_F4) },
+>>>>>>> android-3.18
 	{}
 };
 MODULE_DEVICE_TABLE(pci, fam15h_power_id_table);
@@ -243,6 +254,10 @@ static struct pci_driver fam15h_power_driver = {
 	.name = "fam15h_power",
 	.id_table = fam15h_power_id_table,
 	.probe = fam15h_power_probe,
+<<<<<<< HEAD
+	.remove = __devexit_p(fam15h_power_remove),
+=======
+>>>>>>> android-3.18
 	.resume = fam15h_power_resume,
 };
 
