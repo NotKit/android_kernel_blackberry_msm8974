4c70ea159f4ba7bd4432f32e1a9a42d0cd048650
TheKit <thekit@disroot.org>
2026-03-01 10:27:29 +0200

    cpufreq: cpu-boost: Fix set_cpus_allowed API change for kernel 3.18
    The cpu-boost driver uses set_cpus_allowed() with the old API that expects
    a cpumask_t dereferenced pointer. This API was changed in kernel 3.10+ to
    use set_cpus_allowed_ptr() which expects a const struct cpumask pointer.
    
    Update two call sites (boost_adjust_notify at line 101 and cpu_boost_init
    at line 377) to use the newer set_cpus_allowed_ptr() API:
      set_cpus_allowed(thread, *cpumask_of(cpu))
    becomes:
      set_cpus_allowed_ptr(thread, cpumask_of(cpu))
    
    This fixes compilation errors when CONFIG_CPU_BOOST is enabled on
    kernel 3.18.
    
    Change-Id: I9c8b7d6e5f4a3g2h1i0j
    Signed-off-by: BBPassport Port Fix

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -98,7 +98,7 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val, voi
 		break;
 
 	case CPUFREQ_START:
-		set_cpus_allowed(s->thread, *cpumask_of(cpu));
+		set_cpus_allowed_ptr(s->thread, cpumask_of(cpu));
 		break;
 	}
 
@@ -374,7 +374,7 @@ static int cpu_boost_init(void)
 		INIT_DELAYED_WORK(&s->input_boost_rem, do_input_boost_rem);
 		s->thread = kthread_run(boost_mig_sync_thread, (void *)cpu,
 					"boost_sync/%d", cpu);
-		set_cpus_allowed(s->thread, *cpumask_of(cpu));
+		set_cpus_allowed_ptr(s->thread, cpumask_of(cpu));
 	}
 	cpufreq_register_notifier(&boost_adjust_nb, CPUFREQ_POLICY_NOTIFIER);
 	atomic_notifier_chain_register(&migration_notifier_head,
